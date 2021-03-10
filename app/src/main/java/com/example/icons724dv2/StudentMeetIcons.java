2df9d2fd4a1c3ae0b9e56ea2f3756c29bf1777dc
MPatchell <19mjp12@queensu.ca>
2021-03-09 19:41:38 -0500

    MP Commit1

--- app/src/main/java/com/example/icons724dv2/StudentMeetIcons.java
+++ app/src/main/java/com/example/icons724dv2/StudentMeetIcons.java
@@ -2,7 +2,9 @@ package com.example.icons724dv2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class StudentMeetIcons extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class StudentMeetIcons extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_meet_icons);
+    }
+    public void michaelpatchell(View v) {
+        //launch a new activity
+        Intent O = new Intent(this, michael_patchell.class);
+        startActivity(O);
+
     }
 }
